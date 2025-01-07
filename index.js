a5f9bd887a36d39c4812f6573e802c92ecec0ac4
ArtRosh <artem.rosh17@gmail.com>
2025-01-06 18:52:29 -0800

    function-lab

--- index.js
+++ index.js
@@ -15,6 +15,12 @@ function logWhisper(string) {
 }
 
 function sayHiToHeadphonedRoommate(string) {
-  return string.toUpperCase();
+  if (string === string.toLowerCase()) {
+    return "I can't hear you!";
+  } else if (string === string.toUpperCase()) {
+    return "YES INDEED!";
+  } else if (string === "Let's have dinner together!") {
+    return "I would love to!";
+  }
 }
 
